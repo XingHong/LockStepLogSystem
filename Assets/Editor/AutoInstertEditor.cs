25901e411507ae1d838be404bd20cc028c7a974e
xinghong <xinghonggz@gmail.com>
2022-05-14 17:07:52 +0800

    add function
    添加遍历特定文件夹文件功能

---
+++ Assets/Editor/AutoInstertEditor.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AutoInstertEditor
+{
+    private const string CODE_FILE_ROOT = "TestScript";
+    [MenuItem("AutoInsert/自动插入日志代码")]
+    public static void InsertAction()
+    {
+        string filePath = Application.dataPath + "/" + CODE_FILE_ROOT;
+        DirectoryInfo root = new DirectoryInfo(filePath);
+        List<FileInfo> fileInfoList = new List<FileInfo>();
+        ForeachDir(fileInfoList, root);
+        
+        foreach (FileInfo file in fileInfoList)
+        {
+            string content = ReadFileContent(file.FullName);
+            break;
+        }
+    }
+
+    /// <summary>
+    /// 遍历当前文件夹下的cs文件
+    /// </summary>
+    /// <param name="list"></param>
+    /// <param name="root"></param>
+    private static void ForeachDir(List<FileInfo> list, DirectoryInfo root)
+    {
+        list.AddRange(root.GetFiles("*.cs"));
+        foreach (DirectoryInfo cur in root.GetDirectories())
+        {
+            ForeachDir(list, cur);
+        }
+    }
+
+    private static string ReadFileContent(string path)
+    {
+        string content = string.Empty;
+        using (StreamReader sr = new StreamReader(path))
+        {
+            content = sr.ReadToEnd();
+        }
+        return content;
+    }
+
+    private static void IntertLogCode(string content, string path)
+    { 
+
+    }
+}
---
+++ Assets/TestScript/Test1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test1
+{
+    private int a1;
+    private long a2;
+
+    public Test1()
+    {
+        a1 = 1;
+        a2 = 2;
+    }
+
+    public void SetA1(int val)
+    {
+        a1 = val;
+    }
+}
