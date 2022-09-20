28f25dfc0bfdc45a58e5a3b2742f5fbde8989adf
XingHong <475773414@qq.com>
2022-09-20 17:41:41 +0800

    update FSPDebuger LogTrack function

--- Assets/LogSystem/FSPDebuger.cs
+++ Assets/LogSystem/FSPDebuger.cs
@@ -42,46 +42,88 @@ public class FSPDebuger
         ms_args.Clear();
     }
 
-    public static void LogTrack(ushort hashId, params object[] ps)
+    public static void LogTrack(ushort hashId)
     {
-        ms_items.Add(hashId);
         ms_checksum += hashId;
-        for (int i = 0; i < ps.Length; ++i)
-        {
-            long val = (long)ps[i];
-            ms_args.Add(val);
-            ms_checksum += val;
-        }
+        ms_items.Add(hashId);
     }
 
     public static void LogTrack(ushort hashId, long arg1)
     {
-
+        ms_checksum += hashId;
+        ms_checksum += arg1;
+        ms_items.Add(hashId);
+        ms_args.Add(arg1);
     }
 
     public static void LogTrack(ushort hashId, long arg1, long arg2)
     {
-
+        ms_checksum += hashId;
+        ms_checksum += arg1;
+        ms_checksum += arg2;
+        ms_items.Add(hashId);
+        ms_args.Add(arg1);
+        ms_args.Add(arg2);
     }
 
     public static void LogTrack(ushort hashId, long arg1, long arg2, long arg3)
     {
-
+        ms_checksum += hashId;
+        ms_checksum += arg1;
+        ms_checksum += arg2;
+        ms_checksum += arg3;
+        ms_items.Add(hashId);
+        ms_args.Add(arg1);
+        ms_args.Add(arg2);
+        ms_args.Add(arg3);
     }
 
     public static void LogTrack(ushort hashId, long arg1, long arg2, long arg3, long arg4)
     {
-
+        ms_checksum += hashId;
+        ms_checksum += arg1;
+        ms_checksum += arg2;
+        ms_checksum += arg3;
+        ms_checksum += arg4;
+        ms_items.Add(hashId);
+        ms_args.Add(arg1);
+        ms_args.Add(arg2);
+        ms_args.Add(arg3);
+        ms_args.Add(arg4);
     }
 
     public static void LogTrack(ushort hashId, long arg1, long arg2, long arg3, long arg4, long arg5)
     {
-
+        ms_checksum += hashId;
+        ms_checksum += arg1;
+        ms_checksum += arg2;
+        ms_checksum += arg3;
+        ms_checksum += arg4;
+        ms_checksum += arg5;
+        ms_items.Add(hashId);
+        ms_args.Add(arg1);
+        ms_args.Add(arg2);
+        ms_args.Add(arg3);
+        ms_args.Add(arg4);
+        ms_args.Add(arg5);
     }
 
     public static void LogTrack(ushort hashId, long arg1, long arg2, long arg3, long arg4, long arg5, long arg6)
     {
-
+        ms_checksum += hashId;
+        ms_checksum += arg1;
+        ms_checksum += arg2;
+        ms_checksum += arg3;
+        ms_checksum += arg4;
+        ms_checksum += arg5;
+        ms_checksum += arg6;
+        ms_items.Add(hashId);
+        ms_args.Add(arg1);
+        ms_args.Add(arg2);
+        ms_args.Add(arg3);
+        ms_args.Add(arg4);
+        ms_args.Add(arg5);
+        ms_args.Add(arg6);
     }
 
     public static void IgnoreTrack()
