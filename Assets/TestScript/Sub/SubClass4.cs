9a2da4783c07cead17fda855ae4c798b9d51463d
XingHong <475773414@qq.com>
2022-10-12 14:02:55 +0800

    add test

--- Assets/TestScript/Sub/SubClass4.cs
+++ Assets/TestScript/Sub/SubClass4.cs
@@ -3,8 +3,23 @@ using System.Collections.Generic;
 
 public class SubClass4 : SubClass
 {
-    public void Test4(Fix64 f)
+    public void TestParam1(Fix64 f)
     {
         FSPDebuger.LogTrack(0, f.RawValue);
+    }
+
+    public void TestParam2(int a, int b)
+    {
+        FSPDebuger.LogTrack(0, b);
+    }
+
+    public void TestParam3(int a, long b)
+    {
+        FSPDebuger.LogTrack(0, a, b);
+    }
+
+    public void TestParam4(int a, long b, int c, int d, int e, int f, int g)
+    {
+        FSPDebuger.LogTrack(0, c, d);
     }
 }
