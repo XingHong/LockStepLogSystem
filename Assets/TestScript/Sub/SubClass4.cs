37eec207e4af5113891e96608242de96e5f68182
XingHong <475773414@qq.com>
2022-10-31 14:58:20 +0800

    Update SubClass4.cs

--- Assets/TestScript/Sub/SubClass4.cs
+++ Assets/TestScript/Sub/SubClass4.cs
@@ -1,34 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class SubClass4 : SubClass
+namespace MyTest
 {
-    public void TestParam1(Fix64 f)
-    {FSPDebuger.LogTrack(0, f.RawValue);
-    }
-
-    public void TestParam2(int a, int b)
+    public class SubClass4 : SubClass
     {
-        FSPDebuger.LogTrack(0, b);
-    }
+        public void TestParam1(Fix64 f)
+        {FSPDebuger.LogTrack(0, f.RawValue);
+        }
 
-    public void TestParam3(int a, long b)
-    {
-        FSPDebuger.LogTrack(0, a, b);
-    }
+        public void TestParam2(int a, int b)
+        {
+            FSPDebuger.LogTrack(0, b);
+        }
 
-    public void TestParam4(int a, long b, int c, int d, int e, int f)
-    {
-        FSPDebuger.LogTrack(0, c, d);
-    }
+        public void TestParam3(int a, long b)
+        {
+            FSPDebuger.LogTrack(0, a, b);
+        }
 
-    public void TestParam5(object a, int b)
-    {
+        public void TestParam4(int a, long b, int c, int d, int e, int f)
+        {
+            FSPDebuger.LogTrack(0, c, d);
+        }
 
-    }
+        public void TestParam5(object a, int b)
+        {
 
-    public static void TestParam6(object a, int b)
-    {
- 
+        }
+
+        public static void TestParam6(object a, int b)
+        {
+    
+        }
     }
 }
+
